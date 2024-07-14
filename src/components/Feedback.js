5972ef955ac7777e9deeb33b4c9b53958586b946
Brian <brianhilsden@gmail.com>
2024-07-14 20:04:16 +0300

    added styling

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -45,7 +45,7 @@ function FeedbackForm() {
   };
 
   return (
-    <div style={{backgroundColor: '#f9f9f9', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0,0,0,0.1)' }}>
+    <div style={{backgroundColor: '#f9f9f9', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0,0,0,0.1)',height:"86vh" }}>
     <div className="feedback-form">
       <h2 style={{textAlign: 'center'}}>Feedback Form</h2>
       <form onSubmit={handleSubmit} style={{maxWidth: '500px', margin: '0 auto', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0,0,0,0.1)', backgroundColor: '#f9f9f9'}}>
